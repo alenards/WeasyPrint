0172887738514995d34d70a00899fc7b7b34a8b7
Andy Lenards <alenards@corvusinsurance.com>
2023-08-04 16:10:02 -0700

    Do not require auto appearance on form

--- weasyprint/formatting_structure/boxes.py
+++ weasyprint/formatting_structure/boxes.py
@@ -310,9 +310,8 @@ class Box:
     def is_form(self):
         """Return whether this box is a form."""
         # https://html.spec.whatwg.org/multipage/forms.html#the-form-element
-        if self.style['appearance'] == 'auto' and self.element is not None:
-            if self.element.tag == 'form':
-                return not isinstance(self, (LineBox, TextBox))
+        if self.element is not None and self.element.tag == 'form':
+            return not isinstance(self, (LineBox, TextBox))
         return False
 
     def is_input(self):
